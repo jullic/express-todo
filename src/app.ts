3a2ba77b08ec82ef57f6ef607f9a61d836421425
Александр Львов <79278149+jullic@users.noreply.github.com>
2022-11-22 19:39:18 +0300

    added working with files

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
+import { checkAuth } from './todos/middlewares/check-auth.middleware';
 import { authRouter } from './auth/auth.controller';
 import express, { json } from 'express';
 import mongoose from 'mongoose';
+import multer from 'multer';
 import cors from 'cors';
 
 import { ConfigService } from './config/config.service';
@@ -10,6 +12,14 @@ import { todosRouter } from './todos/todos.controller';
 export class App {
 
 	init() {
+		// create storage for files
+		const storage = multer.diskStorage({
+			destination: 'uploads',
+			filename: (req, file, fn) => {
+				fn(null, file.originalname);
+			}
+		});
+		const upload = multer({ storage });
 		const app = express();
 		const uri = ConfigService.get('MONGO');
 		const port = ConfigService.get('PORT');;
@@ -23,10 +33,18 @@ export class App {
 		app.use(cors());
 		// enabling recognition json
 		app.use(json());
+		// get static file
+		app.use('/uploads', express.static('uploads'))
 		// add auth router
 		app.use('/auth', authRouter);
 		// add todos router
 		app.use('/todos', todosRouter);
+		// upload file
+		app.post('/uploads', checkAuth, upload.single('file'), (req, res) => {
+			res.json({
+				url: `/uploads/${req.file?.originalname}`,
+			})
+		});
 
 		// add error middleware
 		app.use(errorMiddleware);
