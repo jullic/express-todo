7494e5024bf0186bda043f1f022a82cc24b65ea9
Александр Львов <79278149+jullic@users.noreply.github.com>
2022-11-22 13:25:37 +0300

    add mongo connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,15 @@
 import express from 'express';
 import 'dotenv/config';
+import mongoose from 'mongoose';
 
 export class App {
 
 	init() {
+		const uri = process.env.MONGO;
+		mongoose.connect(uri!)
+			.then(() => console.log('Database connection'))
+			.catch(console.log);
+
 		const app = express();
 		const port = process.env.PORT;
 
