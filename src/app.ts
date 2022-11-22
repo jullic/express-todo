bbd4deb8de09bfcdcc38d0c140bab396bf5e198f
Александр Львов <79278149+jullic@users.noreply.github.com>
2022-11-22 15:22:13 +0300

    update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,25 @@
-import express from 'express';
+import { authRouter } from './auth/auth.controller';
+import express, { json } from 'express';
 import mongoose from 'mongoose';
 
 import { ConfigService } from './config/config.service';
+import { errorMiddleware } from './errors/error.middleware';
 
 export class App {
 
 	init() {
+		const app = express();
 		const uri = ConfigService.get('MONGO');
+		const port = ConfigService.get('PORT');;
+
 		mongoose.connect(uri!)
 			.then(() => console.log('Database connection'))
 			.catch(console.log);
 
-		const app = express();
-		const port = ConfigService.get('PORT');;
+		app.use(json());
+		app.use('/auth', authRouter);
+
+		app.use(errorMiddleware);
 
 		app.listen(port, () => {
 			console.log('Server started on port ' + port);
